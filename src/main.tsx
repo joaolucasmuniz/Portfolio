8453d6f3175df96381720b3feaa5390547c42e18
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-07-24 16:39:53 -0300

    feat: page contato

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
@@ -6,9 +5,9 @@ import './index.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
 );
--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,6 +1,138 @@
+import { Box, Grid, IconButton,
+  Link, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 function Contato() {
+  const TELEFONE = '(19) 99746-2506';
+  const EMAIL = 'joaolcmuniz@gmail.com';
+  const LINKEDIN = 'https://www.linkedin.com/in/joao-lucas-muniz';
+  const GITHUB = 'https://github.com/joaolucasmuniz';
+
+  type Copy = {
+    copy: string;
+  };
+  function handleClick(text: Copy) {
+    const { copy } = text;
+    navigator.clipboard.writeText(copy);
+  }
+
   return (
-    <p>Contato</p>
+    <Box
+      sx={
+      {
+        display: 'flex',
+        justifyContent: 'center',
+        height: '93vh',
+      }
+    }
+    >
+      <Grid
+        container
+        sx={
+        {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center',
+        }
+      }
+      >
+        <Grid
+          item
+          xs={ 12 }
+          sm={ 6 }
+          sx={ { display: 'flex',
+            justifyContent: 'center',
+
+          } }
+        >
+          <Typography variant="h2">
+            Onde me encontrar?
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={ 12 }
+          sm={ 6 }
+          sx={ {
+            display: 'flex',
+            justifyContent: { xs: 'center', sm: 'flex-start' },
+
+          } }
+        >
+          <List>
+            <ListItem
+              secondaryAction={
+                <IconButton
+                  onClick={ () => handleClick({ copy: TELEFONE }) }
+                  aria-label="copy"
+                >
+                  <ContentCopyOutlinedIcon />
+                </IconButton>
+          }
+            >
+              <ListItemIcon><LocalPhoneOutlinedIcon /></ListItemIcon>
+              <Link href={ `https://api.whatsapp.com/send?phone=${TELEFONE}&text=Ol%C3%A1%2C%20Jo%C3%A3o%20Lucas%2C%20tudo%20bem%3F%20Gostaria%20de%20conversar%20sobre%20o%20seu%20curr%C3%ADculo.` } target="_blank" rel="noopener noreferrer">
+                <ListItemText primary="WhatsApp" secondary={ TELEFONE } />
+              </Link>
+            </ListItem>
+
+            <ListItem
+              secondaryAction={
+                <IconButton
+                  onClick={ () => handleClick({ copy: EMAIL }) }
+                  aria-label="copy"
+                >
+                  <ContentCopyOutlinedIcon />
+                </IconButton>
+          }
+            >
+              <ListItemIcon><EmailOutlinedIcon /></ListItemIcon>
+              <Link href={ `mailto:${EMAIL}` } target="_blank" rel="noopener noreferrer">
+                <ListItemText primary="E-mail" secondary={ EMAIL } />
+              </Link>
+            </ListItem>
+
+            <ListItem
+              secondaryAction={
+                <IconButton
+                  onClick={ () => handleClick({ copy: LINKEDIN }) }
+                  aria-label="copy"
+                >
+                  <ContentCopyOutlinedIcon />
+                </IconButton>
+          }
+            >
+              <ListItemIcon><LinkedInIcon /></ListItemIcon>
+              <Link href={ LINKEDIN } target="_blank" rel="noopener noreferrer">
+                <ListItemText primary="Linkedin" secondary={ LINKEDIN } />
+              </Link>
+            </ListItem>
+
+            <ListItem
+              secondaryAction={
+                <IconButton
+                  onClick={ () => handleClick({ copy: GITHUB }) }
+                  aria-label="copy"
+                >
+                  <ContentCopyOutlinedIcon />
+                </IconButton>
+          }
+            >
+              <ListItemIcon><GitHubIcon /></ListItemIcon>
+              <Link href={ GITHUB } target="_blank" rel="noopener noreferrer">
+                <ListItemText primary="Github" secondary={ GITHUB } />
+              </Link>
+            </ListItem>
+
+          </List>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 export default Contato;
