a4d6bf634ff7d066c78626c22f1ee9a003c8f51f
joaolucasmuniz <joaolucasmuniz12345@gmail.com>
2023-07-25 11:59:09 -0300

    feat: made part of the projects page

---
+++ src/data/data.ts
@@ -0,0 +1,61 @@
+export const FRONTEND = 'frontend';
+export const BACKEND = 'backend';
+export const FULLSTACK = 'fullstack';
+
+export const data = [
+  {
+    id: 1,
+    type: FRONTEND,
+    name: 'Pixel art',
+    tecnologies: 'HTML, CSS, JavaScrip, Manipulação de DOM.',
+    image: '',
+    url: '',
+    description: 'Objetivo de criar um editor de pixel art com JS.',
+  },
+  {
+    id: 2,
+    type: FRONTEND,
+    name: 'Password manager',
+    tecnologies: 'HTML, CSS, JavaScript',
+    image: '',
+    url: '',
+    description: 'Objetivo de criar um gerenciador de senhas.',
+  },
+  {
+    id: 3,
+    type: FRONTEND,
+    name: 'Trybe Tunes',
+    tecnologies: 'React',
+    image: '',
+    url: '',
+    description: 'Objetivo de criar um player de músicas com funções assíncronas.',
+  },
+  {
+    id: 4,
+    type: FRONTEND,
+    name: 'Trybe-wallet',
+    tecnologies: 'React, Redux',
+    image: '',
+    url: '',
+    description: 'projeto para a prática de React e Redux, com o objetivo de criar um gerenciador de gastos.',
+  },
+  {
+    id: 5,
+    type: FRONTEND,
+    name: 'iChoveu',
+    tecnologies: 'Javascript, HTML, CSS',
+    image: '',
+    url: '',
+    description: 'projeto para a prática de JavaScript assíncrono, em um site que informa a previsão do tempo.',
+  },
+  {
+    id: 6,
+    type: BACKEND,
+    name: 'Store Manager',
+    tecnologies: 'Node, Express, MySQL',
+    image: '',
+    url: '',
+    description: 'projeto para a prática de Node, Express e MySQL, para um gerenciador de produtos.',
+  },
+
+];
--- src/pages/Projetos.tsx
+++ src/pages/Projetos.tsx
@@ -1,7 +1,137 @@
+import { Copyright } from '@mui/icons-material';
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, CssBaseline, Grid, Link, Stack, Typography } from '@mui/material';
+import { BACKEND, FRONTEND, FULLSTACK, data } from '../data/data';
+import { useState } from 'react';
 function Projetos() {
+
+  const cards = data;
+  const [renderCards, setRenderCards] = useState(cards);
+
+  const handleClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const currentValue = e.currentTarget.textContent;
+    switch (currentValue) {
+      case 'Front-end':
+        const newValue = cards.filter((card) => card.type === FRONTEND);
+        setRenderCards(newValue);
+        break;
+      case 'Back-end':
+        const newValue2 = cards.filter((card) => card.type === BACKEND);
+        setRenderCards(newValue2);
+        break;
+      default: setRenderCards(cards);
+    }
+  };
+
+
   return (
     <div>
-      <h1>Projetos</h1>
+        <Box
+          sx={ {
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          } }
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Projetos
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Aqui, compartilho projetos que expressam minha
+              busca por criatividade e inovação.
+              <br />
+              Espero que goste!
+            </Typography>
+            <Stack
+              sx={ { pt: 4 } }
+              direction="row"
+              spacing={ 2 }
+              justifyContent="center"
+            >
+              <Button
+                onClick={ (e) => handleClick(e) }
+                variant="contained"
+              >
+                Front-end
+
+              </Button>
+              <Button
+                onClick={ (e) => handleClick(e) }
+                variant="outlined"
+              >
+                Back-end
+
+              </Button>
+              <Button
+                onClick={ (e) => handleClick(e) }
+                variant="contained"
+              >
+                Todos
+
+              </Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={ { py: 8 } } maxWidth="lg">
+          <Grid container spacing={ 4 }>
+            {renderCards.map((card) => (
+              <Grid item key={ card.id } xs={ 12 } sm={ 6 } md={ 4 }>
+                <Card
+                  sx={ { height: '100%', display: 'flex', flexDirection: 'column' } }
+                >
+                  <CardMedia
+                    component="div"
+                    sx={ {
+                      pt: '50%',
+                    } }
+                    image={ card.image }
+                  />
+                  <CardContent sx={ { flexGrow: 1 } }>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.name}
+                    </Typography>
+                    <Typography>
+                      {card.description}
+                    </Typography>
+                    <Typography variant="body1" component="h2" color={'text.secondary'}>
+                      Tecnologias:
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {card.tecnologies}
+                    </Typography>
+
+                  </CardContent>
+                  <CardActions>
+                    <a href={ card.url}>
+                    <Button size="small" >Ver no github</Button>
+                    </a>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      {/* Footer */}
+      <Box sx={ { bgcolor: 'background.paper', p: 6 } } component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+
+      </Box>
     </div>
   );
 }
